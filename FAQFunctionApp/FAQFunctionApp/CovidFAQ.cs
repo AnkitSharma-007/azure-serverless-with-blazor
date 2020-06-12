dacc2f3387c55ab3f5112e4f42dcc9d252ee27a1
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-06-12 13:25:59 +0530

    first commit

---
+++ FAQFunctionApp/FAQFunctionApp/CovidFAQ.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs;
+using FAQFunctionApp.Models;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Threading.Tasks;
+
+namespace FAQFunctionApp
+{
+    class CovidFAQ
+    {
+        [FunctionName("covidFAQ")]
+        public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+            databaseName:"FAQDB",
+            collectionName:"FAQContainer",
+            ConnectionStringSetting = "DBConnectionString"
+            )] IEnumerable<FAQ> questionSet, 
+        ILogger log)
+        {
+            log.LogInformation("Data fetched from FAQContainer");
+            return new OkObjectResult(questionSet);
+        }
+    }
+}
---
+++ FAQFunctionApp/FAQFunctionApp/Models/FAQ.cs
@@ -0,0 +1,9 @@
+﻿namespace FAQFunctionApp.Models
+{
+    class FAQ
+    {
+        public string Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
